3d778f12c2a80ab0417919c8517091828f021e40
Null <107396951+a-wow@users.noreply.github.com>
2024-10-30 17:04:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-require 'db.php';
+require 'config/db.php';
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: login");
+    header("Location: auth");
     exit;
 }
 
@@ -18,10 +18,12 @@ $stmt = $db->query("
 $posts = $stmt->fetch_all(MYSQLI_ASSOC);
 
 foreach ($posts as &$post) {
-    $stmt = $db->prepare("SELECT comments.content, comments.created_at, users.username, users.profile_image 
-                           FROM comments 
-                           JOIN users ON comments.user_id = users.id 
-                           WHERE comments.post_id = ?");
+    $stmt = $db->prepare("
+        SELECT comments.content, comments.created_at, users.username, users.profile_image 
+        FROM comments 
+        JOIN users ON comments.user_id = users.id 
+        WHERE comments.post_id = ?
+    ");
     $stmt->bind_param("i", $post['id']);
     $stmt->execute();
     $result = $stmt->get_result();
@@ -40,9 +42,12 @@ foreach ($posts as &$post) {
 <body>
     <nav>
         <ul>
-            <li><a href="/">Постервиль</a></li>
-            <li><a href="profile">Настройки</a></li>
-            <li><a href="logout">Выйти</a></li>
+            <li>
+                <img src="uploads/logo/logo.png" alt="Постервиль" style="height: 40px; width: auto;" />
+            </li>
+            <li style="position: absolute; top: 10px; right: 10px;">
+                <button id="logoutBtn" class="logout-button" onclick="window.location.href='logout'">&times;</button>
+            </li>
         </ul>
     </nav>
 
@@ -51,20 +56,24 @@ foreach ($posts as &$post) {
             <div class="post" style="background-image: url('<?= htmlspecialchars($post['background_image']) ?>');">
                 <div class="post-header <?= !empty($post['background_image']) ? 'white-text' : '' ?>">
                     <img src="<?= htmlspecialchars($post['profile_image']) ?>" alt="Profile Image" class="profile-image">
-                    <a href="/@<?= htmlspecialchars($post['username']) ?>" class="<?= !empty($post['background_image']) ? 'white-text' : '' ?>"><?= htmlspecialchars($post['username']) ?></a>
-                    <small class="post-date <?= !empty($post['background_image']) ? 'white-text' : '' ?>"><?= date('H:i', strtotime($post['created_at'])) ?></small>
+                    <a href="/@<?= htmlspecialchars($post['username']) ?>" class="<?= !empty($post['background_image']) ? 'white-text' : '' ?>">
+                        <?= htmlspecialchars($post['username']) ?>
+                    </a>
+                    <small class="post-date <?= !empty($post['background_image']) ? 'white-text' : '' ?>">
+                        <?= date('H:i', strtotime($post['created_at'])) ?>
+                    </small>
                 </div>
-                <p class="<?= !empty($post['background_image']) ? 'white-text' : '' ?>"><?= htmlspecialchars($post['content']) ?></p>
+                <p class="<?= !empty($post['background_image']) ? 'white-text' : '' ?>">
+                    <?= htmlspecialchars($post['content']) ?>
+                </p>
 
-                <!-- Кнопка -->
                 <button class="discussion-btn" data-post-id="<?= $post['id'] ?>">Обсуждение</button>
             </div>
 
-            <!-- Модель комментариев -->
             <div id="modal-<?= $post['id'] ?>" class="modal">
                 <span class="closeModal" style="float:right;cursor:pointer;">&times;</span>
                 <div class="modal-content">
-                    <form class="comment-form" method="post" action="create_comment.php">
+                    <form class="comment-form" method="post" action="functions/create_comment.php">
                         <input type="hidden" name="post_id" value="<?= $post['id'] ?>">
                         <textarea name="content" placeholder="Оставьте комментарий..." required></textarea>
                         <button type="submit">Отправить</button>
@@ -76,8 +85,8 @@ foreach ($posts as &$post) {
                             <div class="comment">
                                 <img src="<?= htmlspecialchars($comment['profile_image']) ?>" alt="Profile Image" class="profile-image">
                                 <div class="comment-content">
-                                <strong><?= htmlspecialchars($comment['username']) ?>:</strong>
-                                <p><?= htmlspecialchars($comment['content']) ?></p>
+                                    <strong><?= htmlspecialchars($comment['username']) ?>:</strong>
+                                    <p><?= htmlspecialchars($comment['content']) ?></p>
                                 </div>
                                 <small class="comment-date"><?= date('H:i', strtotime($comment['created_at'])) ?></small>
                             </div>
@@ -88,9 +97,57 @@ foreach ($posts as &$post) {
         <?php endforeach; ?>
     </div>
 
+    <div id="profileEditModal" class="modal">
+        <div class="modal-content">
+            <span id="closeProfileEditModal" class="closeModal" aria-label="Close Modal">&times;</span>
+            <h2>Редактирование профиля</h2>
+            <form id="profileEditForm" method="post" action="functions/profile.php" enctype="multipart/form-data">
+                <label for="age">Возраст:</label>
+                <input type="number" id="age" name="age" placeholder="Ваш возраст...">
+
+                <label for="country">Страна:</label>
+                <input type="text" id="country" name="country" placeholder="Ваша страна...">
+
+                <label for="email">Почта:</label>
+                <input type="email" id="email" name="email" placeholder="example@mail.com">
+
+                <label for="personal_website">Персональный сайт:</label>
+                <input type="url" id="personal_website" name="personal_website" placeholder="https://example.com">
+
+                <label for="github_profile">GitHub профиль:</label>
+                <input type="url" id="github_profile" name="github_profile" placeholder="https://github.com/username">
+
+                <label for="bio">Информация о себе:</label>
+                <textarea id="bio" name="bio" placeholder="Расскажите о себе..."></textarea>
+
+                <button type="submit">Сохранить изменения</button>
+            </form>
+        </div>
+    </div>
+
+    <div id="imageUploadModal" class="modal">
+        <div class="modal-content">
+            <span id="closeImageUploadModal" class="closeModal" aria-label="Close Modal">&times;</span>
+            <h2>Загрузка изображений</h2>
+
+            <form id="imageUploadForm" method="post" action="functions/profile.php" enctype="multipart/form-data">
+                <label for="profile_image">Изображение профиля:</label>
+                <input type="file" id="profile_image" name="profile_image">
+
+                <label for="background_image">Фоновое изображение:</label>
+                <input type="file" id="background_image" name="background_image">
+
+                <button type="submit">Загрузить изображения</button>
+            </form>
+        </div>
+    </div>
+
+    <button class="edit-profile-btn" id="editProfileBtn">✎</button>
+    <button class="upload-image-btn" id="uploadImageBtn">🖼️</button>
+
     <div id="myModal" class="modal">
         <span id="closeModal" style="float:right;cursor:pointer;">&times;</span>
-        <form id="postForm" method="post" action="create_post.php">
+        <form id="postForm" method="post" action="functions/create_post.php">
             <textarea name="content" placeholder="Что у тебя на уме?" required></textarea>
             <button type="submit">Опубликовать</button>
         </form>
@@ -126,6 +183,22 @@ foreach ($posts as &$post) {
 
         document.getElementById('closeModal').onclick = function() {
             document.getElementById('myModal').style.display = "none";
+        }
+
+        document.getElementById('editProfileBtn').onclick = function() {
+            document.getElementById('profileEditModal').style.display = "block";
+        }
+
+        document.getElementById('closeProfileEditModal').onclick = function() {
+            document.getElementById('profileEditModal').style.display = "none";
+        }
+
+        document.getElementById('uploadImageBtn').onclick = function() {
+            document.getElementById('imageUploadModal').style.display = "block";
+        }
+
+        document.getElementById('closeImageUploadModal').onclick = function() {
+            document.getElementById('imageUploadModal').style.display = "none";
         }
     </script>
 </body>
